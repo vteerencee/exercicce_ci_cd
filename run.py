e3dfdfb12ad37066395be3d9ede4dd5750497c11
vteerencee <125984850+vteerencee@users.noreply.github.com>
2023-12-13 10:24:07 +0100

    Update run.py

--- run.py
+++ run.py
@@ -9,9 +9,17 @@ def main():
     """
     data = import_data()
     data = rename_columns(data)
+    data_sample = sample_data(data)
+    multiply_dataset(data_sample)
+    data = create_total_sepal_column(data)
+    data = create_total_petal_column(data)
 
-    
-def import_data() -> pd.DataFrame:
+    print("Shape du DataFrame : ", data.shape)
+
+    print("Les deux premières lignes du DataFrame : ")
+    print(data.head(2))
+
+def import_data():
     """
     Import csv file as a dataframe
     Output: data [pd.DataFrame]
@@ -22,7 +30,7 @@ def import_data() -> pd.DataFrame:
 
 def rename_columns(data: pd.DataFrame) -> pd.DataFrame:
     """
-    Doc
+    Rename dataframe columns
     """
     data_renamed = data.rename(columns={"sepal.length": 'sepal_length',
                                 "sepal.width": 'sepal_width',
@@ -31,15 +39,62 @@ def rename_columns(data: pd.DataFrame) -> pd.DataFrame:
     
     return data_renamed
 
+def sample_data(data: pd.DataFrame, sample_size: int = 50) -> pd.DataFrame:
+    """
+    Create a sample of the DataFrame with a specified number of rows.
 
-def sample_data(data: pd.DataFrame, n: int = 50) -> pd.DataFrame:
+    Parameters:
+    - data (pd.DataFrame): The input DataFrame.
+    - sample_size (int): The number of rows to include in the sample. Default is 50.
 
-    return data.sample(n)
+    Returns:
+    - sampled_data (pd.DataFrame): The DataFrame containing the sampled data.
+    """
+    sampled_data = data.sample(n=sample_size, random_state=42) #random_state fixe pour assurer la reproductibilité des résultats.
+    return sampled_data  
     
-def multiply_data(data: pd.DataFrame) -> pd.DataFrame:
+def multiply_dataset(data: pd.DataFrame, multiplier: int = 3) -> pd.DataFrame:
+    """
+    Multiply the dataset by a specified factor using pd.concat.
 
-    return pd.concat([data] * 3, ignore_index=True)
+    Parameters:
+    - data (pd.DataFrame): The input DataFrame.
+    - multiplier (int): The factor by which to multiply the dataset. Default is 3.
 
+    Returns:
+    - multiplied_data (pd.DataFrame): The DataFrame containing the multiplied data.
+    """
+    # Utilise pd.concat pour concaténer le DataFrame original plusieurs fois
+    multiplied_data = pd.concat([data] * multiplier, ignore_index=True)
+    return multiplied_data
+
+def create_total_sepal_column(data: pd.DataFrame) -> pd.DataFrame:
+    """
+    Create a new column "total_sepal" in the DataFrame as the sum of "sepal_length" and "sepal_width".
+
+    Parameters:
+    - data (pd.DataFrame): The input DataFrame.
+
+    Returns:
+    - data_with_total_sepal (pd.DataFrame): The DataFrame with the new "total_sepal" column.
+    """
+    # Ajoute une nouvelle colonne "total_sepal" en faisant la somme de "sepal_length" et "sepal_width"
+    data_with_total_sepal = data.assign(total_sepal=data['sepal_length'] + data['sepal_width'])
+    return data_with_total_sepal
+
+def create_total_petal_column(data: pd.DataFrame) -> pd.DataFrame:
+    """
+    Create a new column "total_petal" in the DataFrame as the sum of "petal_length" and "petal_width".
+
+    Parameters:
+    - data (pd.DataFrame): The input DataFrame.
+
+    Returns:
+    - data_with_total_petal (pd.DataFrame): The DataFrame with the new "total_petal" column.
+    """
+    # Ajoute une nouvelle colonne "total_petal" en faisant la somme de "petal_length" et "petal_width"
+    data_with_total_petal = data.assign(total_petal=data['petal_length'] + data['petal_width'])
+    return data_with_total_petal
 
 if __name__ == '__main__':
     """
